85cbc739b8b3dc40d6aec3c2c12939d9d94132e0
wcss <wcss2020@gmail.com>
2019-08-01 11:54:43 +0800

    ...

---
+++ Code/WordTest/WordTest/MainForm.cs
@@ -0,0 +1,135 @@
+﻿using Aspose.Words;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WordTest
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string desktopDir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            string Path_out = Path.Combine(desktopDir, "test_" + DateTime.Now.Ticks + ".docx");
+            string tempFile = Path.Combine(desktopDir, "newtemplete.docx");      //获取模板路径，这个根据个人模板路径而定。
+
+            WordDocument doc = new WordDocument(tempFile);
+
+            Dictionary<string, object> dic = new Dictionary<string, object>();   //创建键值对   第一个string 为书签名称 第二个string为要填充的数据
+            dic.Add("首页密级", "保密");
+            dic.Add("申报领域", "芯片技术");
+            dic.Add("申报方向", "量子芯片技术");
+            dic.Add("项目名称", "量子芯片制造");
+            dic.Add("单位名称", "芯片技术研究院");
+            dic.Add("单位常用名", "芯片技术研究院天津分院");
+            dic.Add("项目负责人", "张三");
+            dic.Add("单位联系人", "张五");
+            dic.Add("联系电话", "68111111");
+            dic.Add("通信地址", "天津滨海区");
+            dic.Add("研究周期", "5");
+            dic.Add("研究经费", "1000");
+            dic.Add("项目关键字", "aaa;bbb;ccc;ddd");
+            
+            doc.insertAllWithBookmark(dic);
+
+            dic = new Dictionary<string, object>();
+            dic.Add("%<test>%", "测试替换...");
+            doc.replaceAllWithBookmark(dic);
+
+            doc.insertDocumentAfterBookMark(new Document(Path.Combine(desktopDir, "ttt.docx")), "基本概念及内涵");
+            doc.insertDocumentAfterBookMark(new Document(Path.Combine(desktopDir, "ttt.docx")), "军事需求分析");
+            doc.insertDocumentAfterBookMark(new Document(Path.Combine(desktopDir, "ttt.docx")), "研究现状");
+            doc.insertDocumentAfterBookMark(new Document(Path.Combine(desktopDir, "ttt.docx")), "研究目标");
+            doc.insertDocumentAfterBookMark(new Document(Path.Combine(desktopDir, "ttt.docx")), "基础性问题");
+
+            doc.WordDoc.UpdateFields();// 更新域
+            doc.WordDoc.UpdateListLabels();
+            doc.WordDoc.UpdatePageLayout();
+            doc.WordDoc.UpdateTableLayout();
+            doc.WordDoc.UpdateThumbnail();
+
+            // Set the appended document to appear on the next page.
+            doc.WordDoc.FirstSection.PageSetup.SectionStart = SectionStart.EvenPage;
+            // Restart the page numbering for the document to be appended.
+            doc.WordDoc.FirstSection.PageSetup.RestartPageNumbering = true;
+
+            // Go to the primary footer
+            //doc.DocBuilder.MoveToHeaderFooter(HeaderFooterType.FooterPrimary);
+            // Add fields for current page number
+            //doc.DocBuilder.InsertField("PAGE", "");
+            // Add any custom text
+            //doc.DocBuilder.Write(" / ");
+            //// Add field for total page numbers in document
+            //doc.DocBuilder.InsertField("NUMPAGES", "");
+
+            doc.DocBuilder.MoveToBookmark("附件3");
+            doc.DocBuilder.InsertBreak(BreakType.SectionBreakNewPage);
+            doc.DocBuilder.PageSetup.Orientation = Aspose.Words.Orientation.Landscape;
+                        
+            //doc.DocBuilder.InsertHtml("<h1>1、分析数据</h1>");
+            //doc.DocBuilder.InsertHtml("<h2>1.1 数据一</h2>");
+            //doc.DocBuilder.InsertHtml("<h2>1.2 数据二</h2>");
+            //doc.DocBuilder.InsertHtml("<h3>1.2.1 计算分析</h3>");
+
+            //doc.DocBuilder.InsertHtml("<h1>2、分析数据</h1>");
+            //doc.DocBuilder.InsertHtml("<h2>2.1 数据一</h2>");
+
+            //Aspose.Words.Lists.List numberedList = doc.WordDoc.Lists.Add(Aspose.Words.Lists.ListTemplate.NumberDefault);
+            //numberedList.ListLevels[1].NumberStyle = NumberStyle.Arabic;
+            //numberedList.ListLevels[1].NumberFormat = "\x0000.\x0001";
+            
+            // Start a numbered list with default formatting.
+            doc.DocBuilder.ListFormat.ApplyNumberDefault();
+            doc.DocBuilder.Writeln("Opening documents from different formats:");
+
+            // Go to second list level, add more text.
+            doc.DocBuilder.ListFormat.ListIndent();
+            doc.DocBuilder.Writeln("DOC");
+            doc.DocBuilder.Writeln("PDF");
+            doc.DocBuilder.Writeln("HTML");
+
+            // Go to second list level, add more text.
+            doc.DocBuilder.ListFormat.ListIndent();
+            doc.DocBuilder.Writeln("DOC");
+            doc.DocBuilder.Writeln("PDF");
+            doc.DocBuilder.Writeln("HTML");
+
+            // Outdent to the first list level.
+            doc.DocBuilder.ListFormat.ListOutdent();
+            doc.DocBuilder.Writeln("Processing documents");
+            doc.DocBuilder.Writeln("Saving documents in different formats:");
+
+            // Indent the list level again.
+            doc.DocBuilder.ListFormat.ListIndent();
+            doc.DocBuilder.Writeln("DOC");
+            doc.DocBuilder.Writeln("PDF");
+            doc.DocBuilder.Writeln("HTML");
+            doc.DocBuilder.Writeln("MHTML");
+            doc.DocBuilder.Writeln("Plain text");
+
+            // Outdent the list level again.
+            doc.DocBuilder.ListFormat.ListOutdent();
+            doc.DocBuilder.Writeln("Doing many other things!");
+
+            // End the numbered list.
+            doc.DocBuilder.ListFormat.RemoveNumbers();
+
+            doc.WordDoc.Save(Path_out); //保存word
+
+            //打开
+            System.Diagnostics.Process.Start(Path_out);
+        }
+    }
+}
---
+++ Code/WordTest/WordTest/WordDocument.cs
@@ -0,0 +1,237 @@
+﻿using Aspose.Words.Replacing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aspose.Words
+{
+    /// <summary>
+    /// 基于Aspose.Words的扩展
+    /// </summary>
+    public class WordDocument
+    {
+        private Document document = null;
+        /// <summary>
+        /// Word文档
+        /// </summary>
+        public Document WordDoc
+        {
+            get { return document; }
+        }
+
+        private DocumentBuilder documentBuilder = null;
+        /// <summary>
+        /// Word文档操作
+        /// </summary>
+        public DocumentBuilder DocBuilder
+        {
+            get { return documentBuilder; }
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="docFile">Word文档</param>
+        public WordDocument(string docFile)
+        {
+            document = new Document(docFile);
+            documentBuilder = new DocumentBuilder(document);
+        }
+
+        /// <summary>
+        /// 插入字典中所有的内容(String或Image)
+        /// </summary>
+        /// <param name="dict">要插入内容的字典（Key=书签,Value=内容）</param>
+        public void insertAllWithBookmark(Dictionary<string, object> dict)
+        {
+            foreach (KeyValuePair<string,object> kvp in dict)   //循环键值对
+            {
+                //移动书签到指定位置
+                DocBuilder.MoveToBookmark(kvp.Key);  //将光标移入书签的位置
+
+                //填充数值
+                if (kvp.Value != null)
+                {
+                    if (kvp.Value.GetType().FullName == typeof(System.Drawing.Image).FullName || kvp.Value.GetType().FullName == typeof(System.Drawing.Bitmap).FullName)
+                    {
+                        documentBuilder.InsertImage((System.Drawing.Image)kvp.Value);
+                    }
+                    else
+                    {
+                        DocBuilder.Write(kvp.Value.ToString());
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 替换字典中所有的内容(String或Image)
+        /// </summary>
+        /// <param name="dict">要替换内容的字典（Key=书签,Value=内容）</param>
+        public void replaceAllWithBookmark(Dictionary<string, object> dict)
+        {
+            foreach (KeyValuePair<string, object> kvp in dict)   //循环键值对
+            {
+                //填充数值
+                if (kvp.Value != null)
+                {
+                    if (kvp.Value.GetType().FullName == typeof(System.Drawing.Image).FullName || kvp.Value.GetType().FullName == typeof(System.Drawing.Bitmap).FullName)
+                    {
+                        System.Text.RegularExpressions.Regex reg = new System.Text.RegularExpressions.Regex(kvp.Key);
+                        WordDoc.Range.Replace(reg, new ReplaceAndInsertImage((System.Drawing.Image)kvp.Value), false);
+                    }
+                    else
+                    {
+                        WordDoc.Range.Replace(kvp.Key, kvp.Value.ToString(), false, false);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 插入文档到书签后
+        /// </summary>
+        /// <param name="tobeInserted"></param>
+        /// <param name="bookmark"></param>
+        /// <returns></returns>
+        public Document insertDocumentAfterBookMark(Document tobeInserted, string bookmark)
+        {
+            // check to be inserted doc
+            if (tobeInserted == null)
+            {
+                return WordDoc;
+            }
+                        
+            // check bookmark and then process
+            if (bookmark != null && bookmark.Trim().Length > 0)
+            {
+                Bookmark bm = WordDoc.Range.Bookmarks[bookmark];
+                if (bm != null)
+                {
+                    documentBuilder.MoveToBookmark(bookmark);
+                    documentBuilder.Writeln();
+                    Node insertAfterNode = documentBuilder.CurrentParagraph.PreviousSibling;
+                    insertDocumentAfterNode(insertAfterNode, tobeInserted);
+                }
+            }
+            else
+            {
+                // if bookmark is not provided, add the document at the end
+                appendDoc(tobeInserted);
+            }
+            return WordDoc;
+        }
+
+        /// <summary>
+        /// 插入文档到节点后
+        /// </summary>
+        /// <param name="insertAfterNode"></param>
+        /// <param name="srcDoc"></param>
+        public void insertDocumentAfterNode(Node insertAfterNode, Document srcDoc)
+        {
+            // Make sure that the node is either a pargraph or table.
+            if ((insertAfterNode.NodeType != NodeType.Paragraph)
+            & (insertAfterNode.NodeType != NodeType.Table))
+                throw new Exception("The destination node should be either a paragraph or table.");
+
+            //We will be inserting into the parent of the destination paragraph.
+            CompositeNode dstStory = insertAfterNode.ParentNode;
+            //Remove empty paragraphs from the end of document
+            while (null != srcDoc.LastSection.Body.LastParagraph && !srcDoc.LastSection.Body.LastParagraph.HasChildNodes)
+            {
+                srcDoc.LastSection.Body.LastParagraph.Remove();
+            }
+
+            NodeImporter importer = new NodeImporter(srcDoc, WordDoc, ImportFormatMode.KeepSourceFormatting);
+            //Loop through all sections in the source document.
+            int sectCount = srcDoc.Sections.Count;
+            for (int sectIndex = 0; sectIndex < sectCount; sectIndex++)
+            {
+                Section srcSection = srcDoc.Sections[sectIndex];
+                //Loop through all block level nodes (paragraphs and tables) in the body of the section.
+                int nodeCount = srcSection.Body.ChildNodes.Count;
+                for (int nodeIndex = 0; nodeIndex < nodeCount; nodeIndex++)
+                {
+                    Node srcNode = srcSection.Body.ChildNodes[nodeIndex];
+                    Node newNode = importer.ImportNode(srcNode, true);
+                    dstStory.InsertAfter(newNode, insertAfterNode);
+                    insertAfterNode = newNode;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// 添加到文档
+        /// </summary>
+        /// <param name="srcDoc"></param>
+        /// <param name="includeSection"></param>
+        public void appendDoc(Document srcDoc, bool includeSection)
+        {
+            // Loop through all sections in the source document.
+            // Section nodes are immediate children of the Document node so we can
+            // just enumerate the Document.
+            if (includeSection)
+            {
+                foreach (Section srcSection in srcDoc.Sections)
+                {
+                    Node dstNode = WordDoc.ImportNode(srcSection, true, ImportFormatMode.UseDestinationStyles);
+                    WordDoc.AppendChild(dstNode);
+                }
+            }
+            else
+            {
+                //find the last paragraph of the last section
+                Node node = WordDoc.LastSection.Body.LastParagraph;
+
+                if (node == null)
+                {
+                    node = new Paragraph(srcDoc);
+                    WordDoc.LastSection.Body.AppendChild(node);
+                }
+
+                if ((node.NodeType != NodeType.Paragraph)
+                & (node.NodeType != NodeType.Table))
+                {
+                    throw new Exception("Use appendDoc(dstDoc, srcDoc, true) instead of appendDoc(dstDoc, srcDoc, false)");
+                }
+                insertDocumentAfterNode(node, srcDoc);
+            }
+        }
+
+        /// <summary>
+        /// 添加到文档
+        /// </summary>
+        /// <param name="srcDoc"></param>
+        public void appendDoc(Document srcDoc)
+        {
+            appendDoc(srcDoc, true);
+        }
+    }
+
+    public class ReplaceAndInsertImage : IReplacingCallback
+    {
+        /// <summary>
+        /// 需要插入的图片
+        /// </summary>
+        public System.Drawing.Image ImageObj { get; set; }
+
+        public ReplaceAndInsertImage(System.Drawing.Image img)
+        {
+            this.ImageObj = img;
+        }
+
+        public ReplaceAction Replacing(ReplacingArgs e)
+        {
+            //获取当前节点
+            var node = e.MatchNode;
+            //获取当前文档
+            Document doc = node.Document as Document;
+            DocumentBuilder builder = new DocumentBuilder(doc);
+            //将光标移动到指定节点
+            builder.MoveTo(node);
+            //插入图片
+            builder.InsertImage(ImageObj);
+            return ReplaceAction.Replace;
+        }
+    }
+}
